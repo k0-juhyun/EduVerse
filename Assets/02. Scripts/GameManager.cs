4802ad238e17e0930ccde17eed67e8700fb03846
k0-juhyun <imkojuhyun@gmail.com>
2023-10-17 18:11:44 +0900

    캐릭터 움직임 구현
    캐릭터 움직임 구현

--- Assets/02. Scripts/GameManager.cs
+++ Assets/02. Scripts/GameManager.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterZoomIn : MonoBehaviour
+// 접속 -> 교사 / 확인
+public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,9 +14,6 @@ public class CharacterZoomIn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount == 2)
-        {
-
-        }
+        
     }
 }
---
+++ Assets/02. Scripts/KJH/CharacterJoyStick.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CharacterJoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    public RectTransform rectBackground;
+    public RectTransform rectJoyStick;
+
+    private float radius;
+    public float moveSpeed;
+
+    public GameObject Character;
+
+    private bool isTouch = false;
+
+    private Vector3 movePos;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isTouch = true;
+    }
+
+    // 손을 떼면
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        // 손 떼면 조이스틱 초기화
+        rectJoyStick.localPosition = Vector3.zero;
+
+        isTouch = false;
+    }
+
+    // 드래그중
+    // 드래그 하는 곳으로 포인터 이동
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 value = eventData.position - (Vector2)rectBackground.position;
+
+        value = Vector2.ClampMagnitude(value, radius);
+
+        rectJoyStick.localPosition = value;
+
+        value = value.normalized;
+
+        //Vector2 dis = 
+
+        // 조이스틱 방향으로 움직임
+        movePos = new Vector3(value.x * moveSpeed * Time.deltaTime, 0, value.y * moveSpeed * Time.deltaTime);
+
+        // 움직이는 방향 바라봄
+        Character.transform.forward = movePos;
+    }
+
+    private void Awake()
+    {
+        radius = rectBackground.rect.width * 0.5f;
+    }
+
+    private void FixedUpdate()
+    {
+        // 터치할때만 움직이도록
+        if (isTouch)
+        {
+            Character.transform.position += movePos;
+        }
+    }
+}
