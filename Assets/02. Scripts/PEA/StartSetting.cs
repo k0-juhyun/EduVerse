1205dda5783fc091fa73f1b4d690afc75f5b07b8
kcheee <cksgml1101@naver.com>
2023-10-22 22:44:44 +0900

    칠판

--- Assets/02. Scripts/PEA/StartSetting.cs
+++ Assets/02. Scripts/PEA/StartSetting.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using UnityEngine.SceneManagement;
 
 public class StartSetting : MonoBehaviour
 {
@@ -19,15 +20,33 @@ public class StartSetting : MonoBehaviour
         
     }
 
+    public IEnumerator LoadLevelAsync(int levelIndex)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelIndex);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Scene이 로드된 후에 실행될 코드
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            // 이 코드는 PhotonNetwork가 연결되어 있고 준비된 상태에서만 실행됩니다.
+            // 이 코드는 씬이 로드된 후에 실행됩니다.
+            NetworkManager.instance.photontext.text = "SCene로드";
+        }
+    }
+
     public void OnCllickStartBtn()
     {
-        if(nameInput.text.Length > 0)
+        if (nameInput.text.Length > 0)
         {
-            PlayerManager.instance.SetMyInfo( new User(nameInput.text, isTeacherTpggle.isOn));
-            if(PhotonNetwork.IsConnectedAndReady)
-            {
-                PhotonNetwork.LoadLevel(1);
-            }
+            PlayerManager.instance.SetMyInfo(new User(nameInput.text, isTeacherTpggle.isOn));
+
+            PhotonNetwork.LoadLevel(1);
+
+            //StartCoroutine(LoadLevelAsync(1));
         }
     }
 }
