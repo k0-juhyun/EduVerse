7ea152bc243fffeab0ccc3cfea546a7a8ecdd60b
k0-juhyun <imkojuhyun@gmail.com>
2023-10-28 20:10:55 +0900

    카메라 드래그 수정

--- Assets/02. Scripts/KJH/CameraSetting.cs
+++ Assets/02. Scripts/KJH/CameraSetting.cs
@@ -78,90 +78,92 @@ public class CameraSetting : MonoBehaviourPun
     {
         if (characterInteraction.isTPSCam)
         {
-            HandleTouchInput();
-            HandleMouseInput();
-            HandleMouseScroll();
-        }
-        else
-        {
-            FPS_Camera.transform.forward = targetTransform.transform.forward;
-        }
-    }
-
-    // 터치 입력 처리
-    private void HandleTouchInput()
-    {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            if (touchZone.Contains(touch.position))
+            if (Input.touchCount > 0)
             {
-                switch (touch.phase)
+                Touch touch = Input.GetTouch(0);
+
+                // 터치 영역 내에서만 터치 이벤트를 처리합니다.
+                if (touchZone.Contains(touch.position))
                 {
-                    case TouchPhase.Moved:
-                        DragCamera(touch.position);
-                        break;
-                    case TouchPhase.Began:
-                        StartDragging(touch.position);
-                        break;
-                    case TouchPhase.Ended:
-                    case TouchPhase.Canceled:
-                        StopDragging();
-                        break;
+                    if (touch.phase == TouchPhase.Moved)
+                    {
+                        // 터치 드래그로 회전
+                        if (isDragging)
+                        {
+                            Vector2 delta = (Vector2)Input.mousePosition - previousMousePosition;
+                            xRotate += delta.x * rotSpeed;
+
+                            // x값 제한
+                            xRotate = Mathf.Clamp(xRotate, -20f, 0f);
+
+                            cameraPivotTransform.localEulerAngles = new Vector3(Mathf.Clamp(0f, 0f, 360f), Mathf.Clamp(xRotate, -20f, 0f), 0);
+                            previousMousePosition = (Vector2)Input.mousePosition;
+                        }
+                    }
+                    else if (touch.phase == TouchPhase.Began)
+                    {
+                        // 터치 시작
+                        if (touch.phase == TouchPhase.Began && touch.tapCount == 1)
+                        {
+                            isDragging = true;
+                            previousMousePosition = touch.position;
+                        }
+                    }
+                    else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+                    {
+                        // 터치 종료
+                        isDragging = false;
+                    }
+
+                    // 터치 확대 및 축소
+                    //if (Input.touchCount == 2)
+                    //{
+                    //    Touch touchZero = Input.GetTouch(0);
+                    //    Touch touchOne = Input.GetTouch(1);
+
+                    //    Vector2 touchZeroPreviousPos = touchZero.position - touchZero.deltaPosition;
+                    //    Vector2 touchOnePreviousPos = touchOne.position - touchOne.deltaPosition;
+
+                    //    float prevMagnitude = (touchZeroPreviousPos - touchOnePreviousPos).magnitude;
+                    //    float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+                    //    float difference = currentMagnitude - prevMagnitude;
+                    //    //ZoomCamera(difference * 0.01f);
+                    //}
                 }
             }
-        }
-    }
 
-    // 마우스 입력 처리
-    private void HandleMouseInput()
-    {
-        if (touchZone.Contains(Input.mousePosition))
-        {
+            // 마우스 입력 처리
             if (Input.GetMouseButtonDown(0))
             {
-                StartDragging(Input.mousePosition);
+                if (touchZone.Contains(Input.mousePosition))
+                {
+                    isDragging = true;
+                    previousMousePosition = Input.mousePosition;
+                }
             }
+
+            // 카메라 드래그 후 손을 뗐을 때
             else if (Input.GetMouseButtonUp(0))
             {
-                StopDragging();
+                isDragging = false;
             }
+
+            if (isDragging)
+            {
+                Vector2 delta = (Vector2)Input.mousePosition - previousMousePosition;
+                xRotate += delta.x * rotSpeed;
+                cameraPivotTransform.localEulerAngles = new Vector3(0, xRotate, 0);
+                previousMousePosition = (Vector2)Input.mousePosition;
+            }
+
+            // 마우스 스크롤로 확대 및 축소
+            //float scroll = Input.GetAxis("Mouse ScrollWheel");
+            //ZoomCamera(scroll);
         }
 
-        if (isDragging)
-        {
-            DragCamera(Input.mousePosition);
-        }
-    }
-
-    // 카메라 드래그
-    private void DragCamera(Vector2 currentMousePosition)
-    {
-        Vector2 delta = currentMousePosition - previousMousePosition;
-        xRotate += delta.x * rotSpeed;
-        cameraPivotTransform.localEulerAngles = new Vector3(0, xRotate, 0);
-        previousMousePosition = currentMousePosition;
-    }
-
-    // 드래그 시작
-    private void StartDragging(Vector2 mousePosition)
-    {
-        isDragging = true;
-        previousMousePosition = mousePosition;
-    }
-
-    // 드래그 중지
-    private void StopDragging()
-    {
-        isDragging = false;
-    }
-
-    // 마우스 스크롤 처리
-    private void HandleMouseScroll()
-    {
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        // ZoomCamera(scroll); // 여기에 원하는 확대/축소 로직을 넣으세요.
+        else
+            FPS_Camera.gameObject.transform.forward = targetTransform.transform.forward;
     }
 
     public void ToggleCameraMode(bool isTPSCam, bool isSitting)
