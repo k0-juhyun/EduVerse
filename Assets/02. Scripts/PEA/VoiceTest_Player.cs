e83cf2829bb1a4345d5bddd51455e2f57c51356e
Park-Eunah <90666966+Park-Eunah@users.noreply.github.com>
2023-11-02 17:54:49 +0900

    포톤 보이스, 학생들 음소거

---
+++ Assets/02. Scripts/PEA/VoiceTest_Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class VoiceTest_Player : MonoBehaviourPun
+{
+    private Photon.Voice.Unity.Recorder recorder;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void MuteStudents(bool useStudenetsMike)
+    {
+        photonView.RPC(nameof(RPCMuteStudents), RpcTarget.Others, useStudenetsMike);
+    }
+
+    [PunRPC]
+    public void RPCMuteStudents(bool useStudentsMike)
+    {
+        Voice.instance.Mute(useStudentsMike);
+    }
+}
