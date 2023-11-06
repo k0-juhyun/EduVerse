b8d73747b25671f9d88e97fa3a094b2f42004d5e
k0-juhyun <imkojuhyun@gmail.com>
2023-11-06 14:50:28 +0900

    닉네임 띄우도록

--- Assets/02. Scripts/KJH/MyPageNickName.cs
+++ Assets/02. Scripts/KJH/MyPageNickName.cs
@@ -3,14 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MyPageNickName : MonoBehaviour
 {
     public Text myNickNameTxt;
     public string myNickName;
 
+    Scene scene;
     void Start()
     {
-        myNickNameTxt.text = PhotonNetwork.NickName + " ¼±»ý´Ô | ·Î±×¾Æ¿ô";
+        myNickNameTxt.text = PhotonNetwork.NickName;
+    }
+
+    private void Update()
+    {
+        if ((scene.buildIndex == 2 && scene.buildIndex == 3) == false)
+            myNickNameTxt.transform.forward = Camera.main.transform.forward;
     }
 }
--- Assets/02. Scripts/KJH/TeachersRoom.cs
+++ Assets/02. Scripts/KJH/TeachersRoom.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TeacherInteraction : MonoBehaviour
+public class TeachersRoom : MonoBehaviour
 {
     public Button Btn;
 
