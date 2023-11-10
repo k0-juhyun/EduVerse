fc9d421d739d224b5c3338ba35e83ffd26f52e18
k0-juhyun <imkojuhyun@gmail.com>
2023-11-10 17:55:34 +0900

    커스텀 씬 작업중

--- Assets/02. Scripts/KJH/MyPageNickName.cs
+++ Assets/02. Scripts/KJH/MyPageNickName.cs
@@ -10,18 +10,9 @@ public class MyPageNickName : MonoBehaviourPun
     public Text myNickNameTxt;
     public string myNickName;
 
-    public Camera cam;
-
-    Scene scene;
     void Start()
     {
-        cam = cam?.GetComponent<Camera>();
         myNickNameTxt.text = PhotonNetwork.NickName;
     }
 
-    private void Update()
-    {
-        if ((scene.buildIndex == 2 && scene.buildIndex == 3) == false && cam != null)
-            myNickNameTxt.transform.forward = cam.transform.forward;
-    }
 }
