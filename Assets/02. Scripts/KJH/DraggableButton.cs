d42392c063e3d02f3297a1f9d939351380a7526a
k0-juhyun <imkojuhyun@gmail.com>
2023-11-15 21:49:21 +0900

    기능 테스트 완성

---
+++ Assets/02. Scripts/KJH/DraggableButton.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DraggableButton : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    private RectTransform rectTransform;
+    private Canvas parentCanvas;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        parentCanvas = GetComponentInParent<Canvas>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (parentCanvas != null)
+        {
+            rectTransform.anchoredPosition += eventData.delta / parentCanvas.scaleFactor;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        SaveButtonPosition();
+    }
+
+    private void SaveButtonPosition()
+    {
+        PlayerPrefs.SetFloat(gameObject.name + "_posX", rectTransform.anchoredPosition.x);
+        PlayerPrefs.SetFloat(gameObject.name + "_posY", rectTransform.anchoredPosition.y);
+        PlayerPrefs.Save();
+    }
+}
