e1d11e209a5ab311d95247345f9bd318a8de6f3d
kcheee <cksgml1101@naver.com>
2023-11-20 05:12:21 +0900

    해설 하기 전

---
+++ Assets/02. Scripts/KCH/Quiz/IncorrectNote.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncorrectNote : QuizMarketManager
+{
+
+
+    public override void unitOnBtnClick(string unit)
+    {
+        base.unitOnBtnClick(unit);
+    }
+}
--- Assets/02. Scripts/KCH/Quiz/StudentQuizDB.cs
+++ Assets/02. Scripts/KCH/Quiz/StudentQuizDB.cs
@@ -21,38 +21,32 @@ public class StudentQuizDB : MonoBehaviour
     public TextMeshProUGUI Unit_4_Average;
     public TextMeshProUGUI Unit_5_Average;
 
+    public GameObject IncorrectNotePanel;
     // student_QuizData에서 참조.
+    // 여기서 문제 가져오면 된다 찬희야 
     public QuizInfo studentQuizinfo;
 
+
+
     // 단원별 오답 정리.
-    private void Update()
+
+    public List<titleinfo> LoadQuizData()
     {
-        if (Input.GetKeyDown(KeyCode.M))
+        // 2단원 틀린 개수만큼 로드
+        foreach (titleinfo titleinfos in studentQuizinfo.Unit_2.IncorrectAnswer)
         {
-
-            //오답의 리스트 개수만큼 titleinfo 리스트에 적용.
-            string a = studentQuizinfo.Unit_5.CorrectAnswer[0].Title;
-
-            // 단원별로 문제 정답 해설 적어두기.
-
-            foreach (titleinfo titleinfos in studentQuizinfo.Unit_2.CorrectAnswer)
-            {
-                Debug.Log("타이틀 : "+titleinfos.Title);
-                Debug.Log("답 : " + titleinfos.Answer);
-                Debug.Log("코멘트 : " + titleinfos.Commentary);
-
-            }
-
-            Unit_1.semicircleTween(0.4f);
-            Unit_2.semicircleTween(0.3f);
-
-            Unit_3.semicircleTween(0.5f);
-
-            Unit_4.semicircleTween(0.2f);
-
-            Debug.Log(a);
-
+            Debug.Log("타이틀 : " + titleinfos.Title);
+            Debug.Log("답 : " + titleinfos.Answer);
+            Debug.Log("코멘트 : " + titleinfos.Commentary);
         }
     }
 
+    public void OnIncorrectNoteBtnClick()
+    {
+        IncorrectNotePanel.SetActive(true);
+    }
+    public void OffIncorrectNoteBtnClick()
+    {
+        IncorrectNotePanel.SetActive(false);
+    }
 }
