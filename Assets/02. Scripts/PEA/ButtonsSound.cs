554fe69e1c37654ce4d67b5c74008fee7f96800c
k0-juhyun <imkojuhyun@gmail.com>
2023-11-27 06:03:46 +0900

    ㅇ

--- Assets/02. Scripts/PEA/ButtonsSound.cs
+++ Assets/02. Scripts/PEA/ButtonsSound.cs
@@ -6,12 +6,17 @@ using UnityEngine.UI;
 public class ButtonsSound : MonoBehaviour
 {
     public Button[] buttons;
+    public Button[] buttons2;
 
     void Start()
     {
         foreach(Button btn in buttons)
         {
-            btn.onClick.AddListener(() => SoundManager.instance?.PlaySFX(SoundManager.SFXClip.Button));
+            btn.onClick?.AddListener(() => SoundManager.instance?.PlaySFX(SoundManager.SFXClip.Button1));
         }
+        //foreach(Button btn in buttons2)
+        //{
+        //    btn.onClick?.AddListener(() => SoundManager.instance?.PlaySFX(SoundManager.SFXClip.Button2));
+        //}
     }
 }
